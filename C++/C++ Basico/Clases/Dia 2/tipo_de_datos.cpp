46c9d6360184c733470cbdffd42cb07ff59f2481
J3xLe1988B3lx0x2E6 <johnb@ciencias.unam.mx>
2022-12-27 19:02:18 -0600

    [INSTRUCTURES][MAIN][FIX] Se agrego informacion en tipo de datos y modifico terminal de gitignoere y cpp

---
+++ C++/C++ Basico/Clases/Dia 2/tipo_de_datos.cpp
@@ -0,0 +1,35 @@
+#include <iostream> //Biblioteca estandard
+
+using namespace std;//Usando namespace para quitar std
+
+int main(int argc, char const *argv[])
+{
+    int short pequentero = 4; //Un entero muy chico.
+    cout<<"Este es un enterito: "<<pequentero<<endl;
+    int entero = 10;
+    cout<<"Este es un entero: "<<entero<<endl;//Tipo de dato entero.
+    int long enterote = 23984;
+    cout<<"Este es un enterote: "<<enterote<<endl;//Tipo de dato entero grandote.
+    float flotante = 0.5; //Podria decir que es un numero real
+    cout<<"Este es un flotante: "<<flotante<<endl; 
+    double flotante_doble = 3.14159265358979323846; //Un numero real grande
+    cout<<"Este es un flotante doble: "<<flotante_doble<<endl;
+    double long muy_doble_flotante = 3.14159265358979323846485910293847; //Un numero real muy grandote.
+    cout<<"Este es un double flotante con mas precision: "<<muy_doble_flotante<<endl;
+    char letra = 'a';//Esto es un caracter.
+    cout<<"Esto es un char: "<<letra<<endl;
+    /*
+        Caso especial: Unsigned.
+        En pocas palabras almacena más que un int con valores positvos
+    */
+    unsigned int sin_signo = -2;
+    //En la linea anterior dara un valor raro ya que solo admite numeros positivos.
+    cout<<"Este es el caso especial con unsigned(valor raro) : "<<sin_signo<<endl;
+    sin_signo = 2;
+    //Ahora si dara el valor de 2.
+    cout<<"Este es el caso especial con unsigned(valor correcto): "<<sin_signo<<endl;
+    bool verdadero = true; //Valores booleanos true o false pero dara resultado 1 o 0
+    cout<<"C++ es bien pro? "<<verdadero<<". Entonces es verdadero."<<endl;
+
+    return 0;
+}
